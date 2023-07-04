5b9b1690550606c3b877de17501608fd6b176c94
Danny Gonzalez <danny.galicia@rhizom.me>
2023-07-04 12:38:25 -0300

    feat : create auth controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { AuthResolver } from './auth/auth.resolver';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService,PrismaService],
+  providers: [AppService,PrismaService, AuthResolver],
 })
 export class AppModule {}
