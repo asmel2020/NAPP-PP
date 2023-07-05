ee305eca85526adbfb20a32cf8c3325b242c3e83
Danny Gonzalez <danny.galicia@rhizom.me>
2023-07-05 16:14:25 -0300

    feat: add jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule]
+  
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService,PrismaService]
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+
+      imports: [],
+      
+      inject: [],
+
+      useFactory: async () => ({
+        secret: 'ASDASD',
+        signOptions: { expiresIn: '60s' },
+      }),
+
+    }),
+  ],
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
