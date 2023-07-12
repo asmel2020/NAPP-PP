550ca68700c0beab181365b7efcab660b1882f9e
Danny Gonzalez <danny.galicia@rhizom.me>
2023-07-12 16:53:37 -0300

    feat : fix bug posgret docker

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
-    port: parseInt(process.env.PORT, 10) || 8080,
+    port: parseInt(process.env.PORT, 10) || 8000,
     jwt:{
         secret: process.env.JWT_SECRET,
         expireIn: process.env.JWT_EXPIRES_IN,
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory, } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
-
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(8080);
+  await app.listen(configService.get<number>('port'));
 
 }
 bootstrap();
