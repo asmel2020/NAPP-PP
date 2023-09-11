9be45fb430e1e94064ad313d901a4978c4136086
Danny Gonzalez <danny.galicia@rhizom.me>
2023-09-11 17:21:37 -0300

    feat :add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: '.env',
     isGlobal: true,
     load: [configuration],
-  })]
+  }),PrismaModule]
 })
 export class AppModule {}
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,3 @@
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 8000,
-    jwt:{
-        secret: process.env.JWT_SECRET,
-        expireIn: process.env.JWT_EXPIRES_IN,
-    }
   });
